adbdd675eb09acab9866805e16277178edbd163e
Durra Alnassar <durrah22@icloud.com>
2026-03-18 22:21:21 +0300

    edit sql

--- aurea_store.sql
+++ aurea_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `aurea_store` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `aurea_store` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `aurea_store`;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
